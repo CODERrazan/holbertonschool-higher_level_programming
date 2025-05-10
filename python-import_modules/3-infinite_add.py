ba1dbc1e7fc237f09d3f543e1870fc5b9b2344f9
razan <razan_8801@icloud.com>
2025-05-10 04:09:01 +0000

    Add pycodestyle-validated infinite addition script

--- python-import_modules/3-infinite_add.py
+++ python-import_modules/3-infinite_add.py
@@ -1,11 +1,14 @@
 #!/usr/bin/python3
 import sys
 
+
 def infinite_add():
-    sum = 0
+    """Sum all command-line arguments."""
+    total = 0
     for arg in sys.argv[1:]:
-        sum += int(arg)
-    print(sum)
+        total += int(arg)
+    print(total)
+
 
 if __name__ == "__main__":
     infinite_add()
