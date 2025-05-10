a843b9d908b44fcf76192a9cce49393b3e4ec93c
razan <razan_8801@icloud.com>
2025-05-10 03:30:55 +0000

    Perfect two-digit combos: pycodestyle valid, meets all requirements

--- python-if_else_loops_functions/6-print_comb3.py
+++ python-if_else_loops_functions/6-print_comb3.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 for first in range(10):
     for second in range(first + 1, 10):
-        print("{}{}".format(first, second), 
+        print("{}{}".format(first, second),
               end=", " if first < 8 or second < 9 else "\n")
