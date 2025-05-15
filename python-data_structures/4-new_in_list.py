8d30923716e339db5c13504407741dbdbc31fe06
razan <razan_8801@icloud.com>
2025-05-15 15:51:55 +0000

    Fix pycodestyle under 80 char

--- python-data_structures/4-new_in_list.py
+++ python-data_structures/4-new_in_list.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def new_in_list(my_list, idx, element):
-    """we cheked then we created copy then if so return copy if not we change"""
+    """we checked then created copy, return copy if not we change"""
     if idx < 0 or idx >= len(my_list):
         return my_list.copy()
     new_list = my_list.copy()
