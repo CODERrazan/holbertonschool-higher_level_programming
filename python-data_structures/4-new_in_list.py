6884b64354a6a09999a240425f3cb63e1dcca57a
razan <razan_8801@icloud.com>
2025-05-15 15:46:38 +0000

    Fix issue with the output task 4

--- python-data_structures/4-new_in_list.py
+++ python-data_structures/4-new_in_list.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 def new_in_list(my_list, idx, element):
-    """we created a copy then we cheked  if so return copy if not we change"""
-    Thecpoy = my_list.copy()
-    if idx < 0 or idx >= len(Thecopy):
-        return new_list
-    Thecopy[idx] = element
-    return Thecopy
+    """ we cheked then we created copy then if so return copy if not we change"""
+    if idx < 0 or idx >= len(my_list):
+        return my_list.copy()
+    new_list = my_list.copy()
+    new_list[idx] = element
+    return new_list
