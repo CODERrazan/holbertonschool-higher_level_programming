5faab177ae7bde02439d506226f6ad52c0db1fe7
razan <razan_8801@icloud.com>
2025-05-16 22:46:38 +0000

    task 3 all test cases

--- python-test_driven_development/4-print_square.py
+++ python-test_driven_development/4-print_square.py
@@ -4,10 +4,10 @@
 
 def print_square(size):
     """Prints a square of given size using # characters.
-    
+
     Args:
         size: Length of the square sides (must be integer >= 0)
-    
+
     Raises:
         TypeError: If size is not an integer
         ValueError: If size is negative
@@ -16,6 +16,6 @@ def print_square(size):
         raise TypeError("size must be an integer")
     if size < 0:
         raise ValueError("size must be >= 0")
-    
+
     for _ in range(size):
         print("#" * size)
