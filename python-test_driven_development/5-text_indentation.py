9f621c5b22b8295dcc5a766064a94f4fb071ad32
razan <razan_8801@icloud.com>
2025-05-16 23:21:58 +0000

    Fix output

--- python-test_driven_development/5-text_indentation.py
+++ python-test_driven_development/5-text_indentation.py
@@ -14,12 +14,15 @@ def text_indentation(text):
     if not isinstance(text, str):
         raise TypeError("text must be a string")
 
+    if text == "":
+        return
+
     i = 0
     while i < len(text):
         if text[i] in ".?:":
             print(text[i], end="\n\n")
             i += 1
-            # Skip spaces after delimiter
+            # Skip spaces immediately after delimiter
             while i < len(text) and text[i] == ' ':
                 i += 1
             continue
