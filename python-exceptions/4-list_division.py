a7dfb420802001038befec972a28ca0658adedec
razan <razan_8801@icloud.com>
2025-05-16 20:47:54 +0000

    update

--- python-exceptions/4-list_division.py
+++ python-exceptions/4-list_division.py
@@ -1,21 +1,21 @@
 #!/usr/bin/python3
 def list_division(my_list_1, my_list_2, list_length):
-    """Divides element by element from two lists"""
-    result = []
+    new_list = []
     for i in range(list_length):
         try:
-            num1 = my_list_1[i]
-            num2 = my_list_2[i]
-            if not isinstance(num1, (int, float)) or not isinstance(num2, (int, float)):
+            a = my_list_1[i]
+            b = my_list_2[i]
+            try:
+                res = a / b
+            except TypeError:
                 print("wrong type")
-                result.append(0)
-                continue
-            division = num1 / num2
-            result.append(division)
-        except ZeroDivisionError:
-            print("division by 0")
-            result.append(0)
+                res = 0
+            except ZeroDivisionError:
+                print("division by 0")
+                res = 0
         except IndexError:
             print("out of range")
-            result.append(0)
-    return result
+            res = 0
+        finally:
+            new_list.append(res)
+    return new_list
