23231c90bdedcca0006130e01499e7ce3c47bfe4
razan <razan_8801@icloud.com>
2025-05-16 19:31:09 +0000

    fixing task 10

--- python-data_structures/10-divisible_by_2.py
+++ python-data_structures/10-divisible_by_2.py
@@ -1,7 +1,4 @@
 #!/usr/bin/python3
 def divisible_by_2(my_list=[]):
-    """Returns a list of True or False based on whether elements are divisible by 2."""
-    return [
-        num % 2 == 0
-        for num in my_list
-    ]
+    """Finds all multiples of 2 in a list"""
+    return [num % 2 == 0 for num in my_list]
