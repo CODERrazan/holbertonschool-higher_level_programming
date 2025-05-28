3ada1eeb792b4a8c63c8624d79db980c67ac9f54
razan <razan_8801@icloud.com>
2025-05-28 10:33:32 +0000

    fix style 33

--- python-inheritance/6-base_geometry.py
+++ python-inheritance/6-base_geometry.py
@@ -6,7 +6,7 @@ class BaseGeometry:
     """Base geometry class with unimplemented area method."""
 
     def area(self):
-        """Raise an exception indicating area() is not implemented.        
+        """Raise an exception indicating area() is not implemented. 
         Raises:
             Exception: Always raises with message 'area() is not implemented'
         """
