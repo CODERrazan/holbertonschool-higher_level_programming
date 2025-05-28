79d867cc424d988f3aab483318759c06d9014a01
razan <razan_8801@icloud.com>
2025-05-28 10:37:33 +0000

    BaseGeometry andf integer_valid

---
+++ python-inheritance/7-base_geometry.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""Defines BaseGeometry class with area and validation methods."""
+
+
+class BaseGeometry:
+    """Base geometry class with area and integer validation."""
+
+    def area(self):
+        """Raise an exception indicating area() is not implemented.
+
+        Raises:
+            Exception: Always raises with message 'area() is not implemented'
+        """
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        """Validate a parameter as a positive integer.
+
+        Args:
+            name (str): The name of the parameter.
+            value (int): The parameter to validate.
+
+        Raises:
+            TypeError: If value is not an integer.
+            ValueError: If value is <= 0.
+        """
+        if type(value) is not int:
+            raise TypeError("{} must be an integer".format(name))
+        if value <= 0:
+            raise ValueError("{} must be greater than 0".format(name))
