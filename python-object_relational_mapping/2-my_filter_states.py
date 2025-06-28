aca7f1322e604b7925e47e31f8054e70e8655c28
razan <razan_8801@icloud.com>
2025-06-28 20:36:20 +0000

    Add task 2 updatee

--- python-object_relational_mapping/2-my_filter_states.py
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -8,38 +8,43 @@ import MySQLdb
 import sys
 
 if __name__ == "__main__":
-    # Get command line arguments
-    username = sys.argv[1]
-    password = sys.argv[2]
-    db_name = sys.argv[3]
-    state_name = sys.argv[4]
+    # Validate argument count
+    if len(sys.argv) != 5:
+        print("Usage: ./2-my_filter_states.py <username> <password> <database> <state_name>")
+        sys.exit(1)
 
-    # Connect to MySQL server
-    db = MySQLdb.connect(
-        host="localhost",
-        port=3306,
-        user=username,
-        passwd=password,
-        db=db_name,
-        charset="utf8"
-    )
+    # Get arguments
+    username, password, db_name, state_name = sys.argv[1:5]
 
-    # Create cursor object
-    cur = db.cursor()
-
-    # Create parameterized query using format
-    query = "SELECT * FROM states WHERE name = %s ORDER BY id ASC"
-    
-    # Execute the query with the state_name as parameter
-    cur.execute(query, (state_name,))
-
-    # Fetch all matching rows
-    rows = cur.fetchall()
-
-    # Display results
-    for row in rows:
-        print(row)
-
-    # Close cursor and connection
-    cur.close()
-    db.close()
+    try:
+        # Connect to MySQL
+        db = MySQLdb.connect(
+            host="localhost",
+            port=3306,
+            user=username,
+            passwd=password,
+            db=db_name,
+            charset="utf8"
+        )
+        
+        # Create cursor
+        cur = db.cursor()
+        
+        # Create query using format() as required
+        query = "SELECT * FROM states WHERE name = '{}' ORDER BY id ASC".format(state_name)
+        
+        # Execute query
+        cur.execute(query)
+        
+        # Fetch and display results
+        for row in cur.fetchall():
+            print(row)
+            
+    except MySQLdb.Error as e:
+        print("MySQL Error:", e)
+    finally:
+        # Clean up
+        if 'cur' in locals():
+            cur.close()
+        if 'db' in locals():
+            db.close()
