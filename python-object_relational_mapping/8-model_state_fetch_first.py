30a333c5856d41489185a429eba0b11125e87d45
razan <razan_8801@icloud.com>
2025-06-28 21:18:47 +0000

    Fetch and print the first State object using SQLAlchemy

---
+++ python-object_relational_mapping/8-model_state_fetch_first.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""
+Prints the first State object from the database hbtn_0e_6_usa.
+"""
+
+import sys
+from sqlalchemy import create_engine
+from sqlalchemy.orm import Session
+from model_state import Base, State
+
+if __name__ == "__main__":
+    username, password, db_name = sys.argv[1:4]
+
+    engine = create_engine(
+        'mysql+mysqldb://{}:{}@localhost/{}'.format(
+            username, password, db_name
+        ),
+        pool_pre_ping=True
+    )
+
+    session = Session(engine)
+    first_state = session.query(State).order_by(State.id).first()
+
+    if first_state:
+        print(f"{first_state.id}: {first_state.name}")
+    else:
+        print("Nothing")
+
+    session.close()
