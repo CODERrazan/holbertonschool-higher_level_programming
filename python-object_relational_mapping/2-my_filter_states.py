07cf0948ae8e704bc209b9ff9debdf2de7f63ace
razan <razan_8801@icloud.com>
2025-06-28 21:10:27 +0000

    Add MySQLdb script to filter states by user input

--- python-object_relational_mapping/2-my_filter_states.py
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -1,36 +1,31 @@
 #!/usr/bin/python3
 """
-Script that takes in an argument and displays all values in the states table
-of hbtn_0e_0_usa where name matches the argument. Uses MySQLdb and user input.
+Displays all values in the states table of hbtn_0e_0_usa where
+name matches the user input. Results sorted by id ascending.
 """
 
 import MySQLdb
 import sys
 
 if __name__ == "__main__":
-    # Get command-line arguments
-    username = sys.argv[1]
-    password = sys.argv[2]
-    database = sys.argv[3]
-    state_name = sys.argv[4]
+    username, password, db_name, state_name = sys.argv[1:5]
 
-    # Connect to MySQL server
     db = MySQLdb.connect(
         host="localhost",
         port=3306,
         user=username,
         passwd=password,
-        db=database,
+        db=db_name,
         charset="utf8"
     )
 
     cur = db.cursor()
-    # Use format() to inject user input directly into SQL query
-    query = "SELECT * FROM states WHERE name = '{}' ORDER BY id ASC".format(state_name)
+    query = (
+        "SELECT * FROM states WHERE name = '{}' "
+        "ORDER BY id ASC".format(state_name)
+    )
     cur.execute(query)
-
-    rows = cur.fetchall()
-    for row in rows:
+    for row in cur.fetchall():
         print(row)
 
     cur.close()
