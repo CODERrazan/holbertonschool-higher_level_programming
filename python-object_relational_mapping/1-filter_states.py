19bdf686edb1843bd361cd3d8b2574b3b92fe1d9
razan <razan_8801@icloud.com>
2025-06-28 20:31:48 +0000

    Add task 1 update

--- python-object_relational_mapping/1-filter_states.py
+++ python-object_relational_mapping/1-filter_states.py
@@ -1,47 +1,42 @@
 #!/usr/bin/python3
 """
-Lists all states with a name starting with N (upper N) from the database hbtn_0e_0_usa.
-The script takes 3 arguments: mysql username, mysql password, and database name.
-Results are sorted in ascending order by states.id and displayed as tuples.
+Script that lists all states with a name starting with N (upper N)
+from the database hbtn_0e_0_usa
 """
+
 import MySQLdb
 import sys
 
-
-def filter_states(username, password, dbname):
-    """
-    Connects to a MySQL database and lists all states where the name starts with 'N',
-    sorted by state ID in ascending order.
-
-    Args:
-        username (str): MySQL username
-        password (str): MySQL password
-        dbname (str): Database name
-    """
-    try:
-        db = MySQLdb.connect(
-            host="localhost",
-            port=3306,
-            user=username,
-            passwd=password,
-            db=dbname,
-            charset="utf8"
-        )
-        with db.cursor() as cursor:
-            cursor.execute("""
-                SELECT * FROM states
-                WHERE name LIKE BINARY 'N%'
-                ORDER BY id ASC
-            """)
-            for state in cursor.fetchall():
-                print(state)
-    except MySQLdb.Error as e:
-        print(f"MySQL Error {e.args[0]}: {e.args[1]}")
-    finally:
-        if 'db' in locals():
-            db.close()
-
-
 if __name__ == "__main__":
-    if len(sys.argv) == 4:
-        filter_states(sys.argv[1], sys.argv[2], sys.argv[3])
+    # Get command line arguments
+    username = sys.argv[1]
+    password = sys.argv[2]
+    db_name = sys.argv[3]
+
+    # Connect to MySQL server
+    db = MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        user=username,
+        passwd=password,
+        db=db_name,
+        charset="utf8"
+    )
+
+    # Create a cursor object
+    cur = db.cursor()
+
+    # Execute the SQL query to find states starting with 'N'
+    query = "SELECT * FROM states WHERE name LIKE BINARY 'N%' ORDER BY id ASC"
+    cur.execute(query)
+
+    # Fetch all the rows
+    rows = cur.fetchall()
+
+    # Print the results
+    for row in rows:
+        print(row)
+
+    # Close cursor and connection
+    cur.close()
+    db.close()
