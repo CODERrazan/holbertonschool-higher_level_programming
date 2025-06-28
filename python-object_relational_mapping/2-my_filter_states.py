0b36d60ef7560d245ea473322fcb61a1e551e0c9
razan <razan_8801@icloud.com>
2025-06-28 20:57:50 +0000

    Add task 2: Filter states by user input

--- python-object_relational_mapping/2-my_filter_states.py
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -8,38 +8,49 @@ import MySQLdb
 import sys
 
 if __name__ == "__main__":
+    # Check for correct number of arguments
+    if len(sys.argv) != 5:
+        print("Usage: ./2-my_filter_states.py <username> <password> <database> <state_name>")
+        sys.exit(1)
+
     # Get command line arguments
     username = sys.argv[1]
     password = sys.argv[2]
     db_name = sys.argv[3]
     state_name = sys.argv[4]
 
-    # Connect to MySQL server
-    db = MySQLdb.connect(
-        host="localhost",
-        port=3306,
-        user=username,
-        passwd=password,
-        db=db_name,
-        charset="utf8"
-    )
+    try:
+        # Connect to MySQL server
+        db = MySQLdb.connect(
+            host="localhost",
+            port=3306,
+            user=username,
+            passwd=password,
+            db=db_name,
+            charset="utf8"
+        )
 
-    # Create cursor object
-    cur = db.cursor()
+        # Create cursor object
+        cur = db.cursor()
 
-    # Create parameterized query using format
-    query = "SELECT * FROM states WHERE name = %s ORDER BY id ASC"
-    
-    # Execute the query with the state_name as parameter
-    cur.execute(query, (state_name,))
+        # Create query with parameterized input
+        query = "SELECT * FROM states WHERE name LIKE BINARY %s ORDER BY id ASC"
+        
+        # Execute the query with the state_name as parameter
+        cur.execute(query, (state_name,))
 
-    # Fetch all matching rows
-    rows = cur.fetchall()
+        # Fetch all matching rows
+        rows = cur.fetchall()
 
-    # Display results
-    for row in rows:
-        print(row)
+        # Display results
+        for row in rows:
+            print(row)
 
-    # Close cursor and connection
-    cur.close()
-    db.close()
+    except MySQLdb.Error as e:
+        print("MySQL Error:", e)
+    finally:
+        # Close cursor and connection if they exist
+        if 'cur' in locals():
+            cur.close()
+        if 'db' in locals():
+            db.close()
