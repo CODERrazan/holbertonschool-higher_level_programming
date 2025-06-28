30364ddc6514a3df64fc1cc8e9dc785d722a2cea
razan <razan_8801@icloud.com>
2025-06-28 21:05:04 +0000

    Add script to filter states by name using MySQLdb

--- python-object_relational_mapping/2-my_filter_states.py
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -1,17 +1,17 @@
 #!/usr/bin/python3
 """
-Script that filters states by user input from hbtn_0e_0_usa database
-using string formatting for the SQL query.
+Script that takes in an argument and displays all values in the states table
+of hbtn_0e_0_usa where name matches the argument. Uses MySQLdb and user input.
 """
 
 import MySQLdb
 import sys
 
 if __name__ == "__main__":
-    # Get command line arguments
+    # Get command-line arguments
     username = sys.argv[1]
     password = sys.argv[2]
-    db_name = sys.argv[3]
+    database = sys.argv[3]
     state_name = sys.argv[4]
 
     # Connect to MySQL server
@@ -20,26 +20,18 @@ if __name__ == "__main__":
         port=3306,
         user=username,
         passwd=password,
-        db=db_name,
+        db=database,
         charset="utf8"
     )
 
-    # Create cursor object
     cur = db.cursor()
-
-    # Create SQL query using string formatting as required
+    # Use format() to inject user input directly into SQL query
     query = "SELECT * FROM states WHERE name = '{}' ORDER BY id ASC".format(state_name)
-    
-    # Execute the query
     cur.execute(query)
 
-    # Fetch all matching rows
     rows = cur.fetchall()
-
-    # Display results
     for row in rows:
         print(row)
 
-    # Close cursor and connection
     cur.close()
     db.close()
