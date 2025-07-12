eb6716892ec382605dfb0eca0c5492b5b1e7bb1f
razan <razan_8801@icloud.com>
2025-07-12 00:40:33 +0000

    added the task 7

--- javascript-warm_up/7-multi_c.js
+++ javascript-warm_up/7-multi_c.js
@@ -2,8 +2,8 @@
 
 const x = parseInt(process.argv[2]);
 
-if (isNaN(x)) {
-  console.log('Missing number of occurrences');
+if (isNaN(x) || x <= 0) {
+  if (isNaN(x)) console.log('Missing number of occurrences');
 } else {
   for (let i = 0; i < x; i++) {
     console.log('C is fun');
